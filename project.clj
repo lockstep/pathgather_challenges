a38df4c39c6d74aa70f39af34e8fcb38e8e97bd6
Maxim Kashchenko <mkaschenko@me.com>
2016-07-07 10:26:22 +0700

    Fetch the entire catalogue with specific fields

--- project.clj
+++ project.clj
@@ -1,2 +1,4 @@
 (defproject pathgather-challenges "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.9.0-alpha9"]])
+  :dependencies [[clj-http "2.2.0"]
+                 [org.clojure/clojure "1.9.0-alpha9"]
+                 [org.clojure/data.json "0.2.6"]])
--- src/pathgather_challenges/core.clj
+++ src/pathgather_challenges/core.clj
@@ -1,1 +1,21 @@
-(ns pathgather-challenges.core)
+(ns pathgather-challenges.core
+  (:require [clj-http.client :as client]
+            [clojure.data.json :as json]
+            [clojure.string :as string]))
+
+(def fields ["name" "description" "slug" "startDate"])
+
+(defn fetch-courses [start limit fields]
+  (let [response (client/get "https://api.coursera.org/api/courses.v1"
+                             {:query-params {:start  start
+                                             :limit  limit
+                                             :fields (string/join "," fields)}})]
+    (get (json/read-str (:body response)) "elements")))
+
+(defn fetch-all-courses
+  ([] (fetch-all-courses 0 100 fields []))
+  ([start limit fields acc]
+   (let [courses (fetch-courses start limit fields)]
+     (if (empty? courses)
+       acc
+       (fetch-all-courses (+ start limit) limit fields (into acc courses))))))
